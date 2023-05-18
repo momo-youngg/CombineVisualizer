3095348e3a635848b3b9ac9fcd2c305bbc715b8d
momo <yeongmoj94+dev@gmail.com>
2023-05-18 21:49:43 +0900

    feat: Entity 1차 구현

---
+++ CombineVisualizerApplication/CombineVisualizerApplication/Entity/CombineGroup.swift
@@ -0,0 +1,64 @@
+//
+//  CombineGroup.swift
+//  CombineVisualizerApplication
+//
+//  Created by momo on 2023/05/18.
+//
+
+import Foundation
+
+struct CombineGroup {
+    let uuid: UUID
+    var elements: [CombineElement]
+    var totalEdgesCount: Int {
+        elements.map { $0.edges.count }.reduce(0) { $0 + $1 }
+    }
+    var subjects: [CombineElement] {
+        elements.filter { $0.elementType == .subject }
+    }
+    var publishers: [CombineElement] {
+        elements.filter { $0.elementType == .publisher }
+    }
+    var subscriptions: [CombineElement] {
+        elements.filter { $0.elementType == .subscription }
+    }
+    var subscribers: [CombineElement] {
+        elements.filter { $0.elementType == .subscriber }
+    }
+}
+
+struct CombineElement {
+    let elementType: ElementType
+    let typeName: String
+    let edges: [Edges]
+}
+
+enum ElementType {
+    case subject
+    case publisher
+    case subscription
+    case subscriber
+}
+
+struct Edges {
+    let sequence: Int
+    let queue: String
+    let thread: String
+    let method: ElementMethod
+}
+
+enum ElementMethod {
+    // Publisher + Subject
+    case receiveSubscriber(String)
+    // Subject
+    case sendOutput
+    case sendCompletion
+    case sendSubscription
+    // Subscriber
+    case receiveSubscription(String)
+    case receiveInput
+    case receiveCompletion
+    // Subscription
+    case request
+    case cancel
+}
---
+++ CombineVisualizerApplication/CombineVisualizerApplication/View/SingleCombineElementView.swift
@@ -0,0 +1,20 @@
+//
+//  SingleCombineElementView.swift
+//  CombineVisualizerApplication
+//
+//  Created by momo on 2023/05/18.
+//
+
+import SwiftUI
+
+struct SingleCombineElementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SingleCombineElementView_Previews: PreviewProvider {
+    static var previews: some View {
+        SingleCombineElementView()
+    }
+}
